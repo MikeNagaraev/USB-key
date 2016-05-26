8c0d6570d9112d918afd0ae1c5703c39ad6f0c91
MikhailLU <clowfinger13@gmail.com>
2016-05-26 12:12:32 +0300

    Block unblock

--- Linux/block.sh
+++ Linux/block.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-# Disable keyboard, mouse, in 5 seconds reboot system
+#! /bin/bash
 
 mousedevice='FocalTechPS/2 FocalTech FocalTech Touchpad'
 keydevice='AT Translated Set 2 keyboard'
@@ -7,4 +6,4 @@ keydevice='AT Translated Set 2 keyboard'
     #Disable keyboard and mouse
     xinput --disable "$mousedevice"
     xinput --disable "$keydevice"
-
+   
